c692d479b67e20d4aa421f45fabc2febe8d1a94d
Arunava Chakraborty <arunavaskar@outlook.com>
2020-01-21 00:47:39 +0530

    done

--- tictactoe.js
+++ tictactoe.js
@@ -13,4 +13,29 @@ function clearBoard(){
 restart.addEventListener('click', clearBoard);
 // check the square marker
 
+var cellOne = document.querySelector("#one");
+
+cellOne.addEventListener("click", function(){
+  // if the cellOne content is blank then
+  // this will set the content to "X"
+  if (cellOne.textContent === '') {
+    cellOne.textContent = 'X';
+  // if the cellOne content is "X" then
+  // this will set the content to "O"
+  }else if (cellOne.textContent === 'X') {
+    cellOne.textContent = "O";
+  // if none of those are true then
+  // set the content to "" blank
+  }else {
+    cellOne.textContent = '';
+  }
+})
+
+
+
+
+
+
+
+
 // for loop to add even listners to all the squares
